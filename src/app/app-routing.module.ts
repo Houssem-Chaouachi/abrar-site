4b84e699599937a3200459903b799aa5f98e1b67
houssem <houssemm09@gmail.com>
2020-11-19 09:11:02 +0100

    roote works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,17 @@ import { CompaniesComponent } from './companies/companies.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AfcComponent } from './afc/afc.component';
 import { CompetenceAfcComponent } from './afc/competence-afc/competence-afc.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-  // { path: '', component: CompaniesComponent},
+  { path: '', component: CompaniesComponent},
   { path: 'about-us', component: AboutUsComponent},
   { path: 'contact-us', component: ContactUsComponent},
-  { path: '1', component: AfcComponent},
+  { path: 'afc', component: AfcComponent},
+  // { path: '', component: NavbarComponent},
+  // { path: 'Companies' , component : CompaniesComponent},
   { path: 'competenceAfc', component: CompetenceAfcComponent},
-  { path: '**', redirectTo: '1'}
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
